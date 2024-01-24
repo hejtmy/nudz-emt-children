6eb7f0bdc2be7713b00d3da461b3a49e73f5d83d
Lukáš Hejtmánek <hejtmy@gmail.com>
2024-01-24 10:18:09 +0100

    adds loading adult data

--- scripts/loading-adults.R
+++ scripts/loading-adults.R
@@ -5,13 +5,16 @@ df_rbans <- read.table("data/df_rbans_a.csv", sep = ";", header = TRUE)
 df_emt_adults <- read.table("data/df_emt_adults.csv", sep = ";", header = TRUE)
 
 df_emt_adults <- df_emt_adults %>%
-    select(-c(X, Pořadí, PC.hry, Tech..Vz.))
+  select(-c(Pořadí, PC.hry, Tech..Vz.))
 
 View(df_emt_adults)
-df_emt_adults %>%
-    pivot_longer(cols = X5por:X11poz, names_to = "task",
-        values_to = "errors") %>%
-    mutate(task = gsub("X", "", task)) %>%
-    separate(task, into = c("task_number", "task_name"),
-        sep = "(?<=\\d)(?=\\D)") %>%
-    separate(ID_NUDZ, into = c("Project", "Session", "ID"), sep = "_")
+
+df_emt_adults <- df_emt_adults %>%
+  pivot_longer(cols = X5por:X11poz, names_to = "task",
+               values_to = "errors") %>%
+  mutate(task = gsub("X", "", task)) %>%
+  separate(task, into = c("task_number", "task_name"),
+           sep = "(?<=\\d)(?=\\D)") %>%
+  separate(ID_NUDZ, into = c("Project", "Session", "ID"), sep = "_")
+
+
