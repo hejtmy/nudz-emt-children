77da13adec0ff407ebe21cbaa24fb6377ddbb79c
Lukáš Hejtmánek <hejtmy@gmail.com>
2024-01-26 16:47:33 +0100

    adds emt adults quarto a loading preprocessing

--- scripts/loading-adults.R
+++ scripts/loading-adults.R
@@ -5,16 +5,26 @@ df_rbans <- read.table("data/df_rbans_a.csv", sep = ";", header = TRUE)
 df_emt_adults <- read.table("data/df_emt_adults.csv", sep = ";", header = TRUE)
 
 df_emt_adults <- df_emt_adults %>%
-  select(-c(Pořadí, PC.hry, Tech..Vz.))
-
-View(df_emt_adults)
-
-df_emt_adults <- df_emt_adults %>%
+  select(-c(Pořadí, PC.hry, Tech..Vz.)) %>%
   pivot_longer(cols = X5por:X11poz, names_to = "task",
                values_to = "errors") %>%
   mutate(task = gsub("X", "", task)) %>%
   separate(task, into = c("task_number", "task_name"),
            sep = "(?<=\\d)(?=\\D)") %>%
-  separate(ID_NUDZ, into = c("Project", "Session", "ID"), sep = "_")
+  separate(ID_NUDZ, into = c("Project", "Session", "ID"), sep = "_") %>%
+  readr::type_convert()
 
+df_rbans <- df_rbans %>%
+  mutate(bindingid = gsub("_", "", ID)) %>%
+  filter(grepl("HCE", bindingid))
 
+df_emt_adults <- df_emt_adults %>%
+  mutate(bindingid = paste0(Project, ID)) %>%
+  filter(grepl("HCE", bindingid))
+
+df_emt_adults %>%
+  left_join(df_rbans, by = "bindingid") %>%
+  filter(Environment == "House", task_number == 5) %>%
+  group_by(task_name, Environment) %>%
+  group_modify(~broom::tidy(cor.test(.$RBANS.A.1, .$errors, method = "spearman"))) %>%
+  select(estimate, statistic, p.value)
