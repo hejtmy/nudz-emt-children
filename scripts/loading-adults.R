6b8acdc1f3fffe3297d28e11385ebf88542a6df2
Lukáš Hejtmánek <hejtmy@gmail.com>
2024-01-28 17:00:21 +0100

    Updates reports and quarto code

--- scripts/loading-adults.R
+++ scripts/loading-adults.R
@@ -1,11 +1,22 @@
 library(dplyr)
 library(tidyr)
+library(here)
+i_am("scripts/loading-children.R")
 
-df_rbans <- read.table("data/df_rbans_a.csv", sep = ";", header = TRUE)
-df_emt_adults <- read.table("data/df_emt_adults.csv", sep = ";", header = TRUE)
+df_rbans <- read.table(here("data/df_rbans_a.csv"), sep = ";", header = TRUE)
+df_emt_adults <- read.table(here("data/df_emt_adults.csv"), sep = ";", header = TRUE)
+
+# rename colums age to asd
+df_emt_adults <- df_emt_adults %>%
+  rename("Age" = "Věk", "Technical_education" = "Tech..Vz.",
+         "Gender" = "Pohlaví")
+df_emt_adults <- df_emt_adults %>%
+  # relabel Gender "zena" is "female"
+  mutate(Gender = case_when(Gender == "žena" ~ "Female",
+                            Gender == "muž" ~ "Male"))
 
 df_emt_adults <- df_emt_adults %>%
-  select(-c(Pořadí, PC.hry, Tech..Vz.)) %>%
+  select(-c(Pořadí, PC.hry, Technical_education)) %>%
   pivot_longer(cols = X5por:X11poz, names_to = "task",
                values_to = "errors") %>%
   mutate(task = gsub("X", "", task)) %>%
@@ -17,6 +28,7 @@ df_emt_adults <- df_emt_adults %>%
          made_error = relative_error_value > 0) %>%
   mutate(error_type = case_when(error_type == "por" ~ "incorrect_order",
                                 error_type == "poz" ~ "incorect_placement"))
+
 
 df_rbans <- df_rbans %>%
   mutate(bindingid = gsub("_", "", ID)) %>%
