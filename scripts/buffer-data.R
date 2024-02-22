48212216fbd95716accd60e917b98eaf421f1370
Lukáš Hejtmánek <hejtmy@gmail.com>
2024-02-22 17:26:29 +0100

    Adds basic senior loading

--- scripts/buffer-data.R
+++ scripts/buffer-data.R
@@ -11,8 +11,13 @@ drive_download(file = as_id(file_id), path = "data/processed-data.xlsx")
 
 ## Adults results
 df_emt_adults <- read_sheet("1sE3a7nEeaB-6norUF9QCgvfZpYdbPLTGBgVnExR1poc",
-    sheet = "ENVIRONMENTS")
-write.table(df_emt_adults, "data/df_emt_adults.csv", row.names = FALSE, sep = ";")
+                            sheet = "ENVIRONMENTS")
+write.table(df_emt_adults, "data/df_emt_adults.csv", row.names = FALSE,
+            sep = ";")
 df_rbans_a <- read_sheet("1N11offQ6y2SavR-PmYQJgerzMlEqkjVkXvWfyazH1t8",
-    sheet = "RBANS.A", na = c("", "X"))
+                         sheet = "RBANS.A", na = c("", "X"))
 write.table(df_rbans_a, "data/df_rbans_a.csv", row.names = FALSE, sep = ";")
+
+## Senior results
+drive_download(file = as_id("1068tUCXQc2ZgBH7K_q7LBGHlEC-xHYPS"),
+               path = "data/senior-data.xlsx")
---
+++ scripts/loading-seniors.R
@@ -0,0 +1,4 @@
+library(here)
+library(readxl)
+i_am("scripts/loading-seniors.R")
+
